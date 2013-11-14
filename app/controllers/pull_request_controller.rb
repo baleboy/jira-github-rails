a7f2f6dbbfc6c62958aaa1b80a2ac7c5af784401
baleboy <bale@balenet.com>
2013-11-14 22:39:09 +0200

    Parse pull request

--- app/controllers/pull_request_controller.rb
+++ app/controllers/pull_request_controller.rb
@@ -5,6 +5,19 @@ class PullRequestController < ApplicationController
   def handle
         
     payload = ActiveSupport::JSON.decode params[:payload]
+
+    projects = ["XWALK", "TEST"]
+    r = Regexp.new("(fixe?[s|d] ?|resolve[s|d]? ?|close[s|d]? ?)?(#{projects.join('|')})-([0-9]+)", Regexp::IGNORECASE)
+    pull_request = payload["pull_request"]
+    
+    pull_request["body"].scan(r) do |match| 
+      
+      should_resolve = !match[0].nil?
+      bug_id = match[1] + '-' + match[2]
+      comment = "#{should_resolve ? 'Will be resolved by' : 'Mentioned by'} " \
+        "[PR ##{payload["number"]}|#{pull_request["html_url"]}] on branch #{pull_request["head"]["ref"]}"
+      Rails.logger.debug comment
+    end
     
     render :nothing => true, :status => 200
     
