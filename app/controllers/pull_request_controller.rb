8e6f9dd062df595a92027dcb3d3e5db3f93b3ee4
baleboy <bale@balenet.com>
2013-11-15 19:05:22 +0200

    Add icons to comment

--- app/controllers/pull_request_controller.rb
+++ app/controllers/pull_request_controller.rb
@@ -33,19 +33,25 @@ class PullRequestController < ApplicationController
     user = pull_request["user"]
     repo = pull_request["head"]["repo"]
 
+    user_string = "[#{user["login"]}|#{user["html_url"]}]"
+    pr_string = "[Pull Request ##{payload["number"]}|#{pull_request["html_url"]}]"
+    
     rc = Jira4R::V2::RemoteComment.new
-    rc.body = "_[#{user["login"]}|#{user["html_url"]}] referenced this issue in project [#{repo["name"]}|#{repo["html_url"]}]_\n\n"\
-      "*[Pull request ##{payload["number"]}|#{pull_request["html_url"]}]:* #{pull_request["title"]}"
-    # pr_details = "[PR ##{payload["number"]}|#{pull_request["html_url"]}] on branch #{pull_request["head"]["ref"]}"
+    
+    if payload["action"] == "opened"
+      rc.body = "(i) #{user_string} referenced this issue in project [#{repo["name"]}|#{repo["html_url"]}]:\n\n"\
+        "*#{pr_string}:* _\"#{pull_request["title"]}\"_"
+    else
+      rc.body = "(/) #{user_string} resolved this issue with *#{pr_string}*"
+    end
     
     pull_request["body"].scan(r) do |match| 
       
-      should_resolve = !match[0].nil?
+      should_resolve = !match[0].nil? && payload["action"] == "closed"
       bug_id = match[2] + '-' + match[3]
-      # rc.body = "#{should_resolve ? 'Will be resolved by' : 'Mentioned by'} " + pr_details
         
       Rails.logger.debug bug_id + ' ' + rc.body
-      
+
       # Comment on this issue
       begin
         @jira_connection.addComment(bug_id, rc)
@@ -54,11 +60,27 @@ class PullRequestController < ApplicationController
       else
         Rails.logger.debug("Successfully added comment to issue %s" % [bug_id])
       end
+      
+      # Close the issue if needed
+      if should_resolve
+        begin
+          available_actions = @jira_connection.getAvailableActions bug_id
+          resolve_action = available_actions.find {|s| s.name == 'Resolve Issue'}
+          if !resolve_action.nil?
+            @jira_connection.progressWorkflowAction(bug_id, resolve_action.id.to_s, [])
+          else
+            Rails.logger.debug("Not allowed to resolve issue %s. Allowable actions: %s" % [bug_id, (available_actions.map {|s| s.name}).to_s])
+          end
+        rescue StandardError => e
+          Rails.logger.error("Failed to resolve issue %s : %s" % [bug_id, e.to_s])
+        else
+          Rails.logger.debug("Successfully resolved issue %s" % [bug_id])
+        end
+      end
     end
     
     render :nothing => true, :status => 200
     
   end
   
-
 end
