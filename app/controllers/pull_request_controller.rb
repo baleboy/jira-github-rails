aea9b8062c10a21d8097e5f041b59239c7a37013
baleboy <bale@balenet.com>
2013-11-17 00:14:08 +0200

    Move Jira-related functions to modules

--- app/controllers/pull_request_controller.rb
+++ app/controllers/pull_request_controller.rb
@@ -1,86 +1,38 @@
-require 'jira4r'
-require 'yaml'
+require 'modules'
 
 class PullRequestController < ApplicationController
+
+  include Modules
   
   respond_to :json
 
-  OPENED_COMMENT_TEMPLATE =
-  "(i) [%{user_id}|%{user_url}] referenced this issue in project [%{repo_name}|%{repo_url}]:\n\n"\
-  "*[Pull Request %{pr_number}|%{pr_url}]* _\"%{pr_title}\"_"
-  
-  CLOSED_COMMENT_TEMPLATE = 
-    "(/) [%{user_id}|%{user_url}] resolved this issue with *[Pull Request %{pr_number}|%{pr_url}]*"
-    
-  FIND_ISSUE_REGEXP_TEMPLATE = 
-    "(fixe?[s|d] *|resolve[s|d]? *|close[s|d]? *)?(http[^ ]+/)?(%{projects})-([0-9]+)"
-      
-
-  def initialize
-        
-    @jira_config = YAML.load(File.new "config/config.yml", 'r')
-    @jira_projects = @jira_config['projects']
-    @jira_connection = Jira4R::JiraTool.new(2, @jira_config['address'])
-
-    # Optional SSL parameters
-    if @jira_config['ssl_version'] != nil
-      @jira_connection.driver.streamhandler.client.ssl_config.ssl_version = @jira_config['ssl_version']
-    end
-    if @jira_config['verify_certificate'] == false
-      @jira_connection.driver.options['protocol.http.ssl_config.verify_mode'] = OpenSSL::SSL::VERIFY_NONE
-    end
-    
-    @jira_connection.login(@jira_config['username'], @jira_config['password'])
-  end
-  
   def handle
-        
-    rc = Jira4R::V2::RemoteComment.new
+
     payload = ActiveSupport::JSON.decode params[:payload]
     
     if payload["action"] == "opened"
-      rc.body = opened_comment_from payload
+      comment = opened_comment_from payload
     else # "closed"
-      rc.body = closed_comment_from payload
+      comment = closed_comment_from payload
     end
           
-    payload["pull_request"]["body"].scan find_issue_regexp do |match| 
-      
-      resolve_issue = !match[0].nil? && payload["action"] == "closed"
-      issue_id = match[2] + '-' + match[3]
+    payload["pull_request"]["body"].scan JiraHelper.instance.find_issue_regexp do |match| 
 
-      # Comment on this issue
-      begin
-        @jira_connection.addComment issue_id, rc 
-      rescue
-        Rails.logger.error "Failed to add comment to issue #{issue_id}"
-      else
-        Rails.logger.debug "Successfully added comment to issue #{issue_id}"
-      end
+      issue_id = regexp_issue_id match
       
-      # Close the issue if needed
-      if resolve_issue
-        begin
-          available_actions = @jira_connection.getAvailableActions issue_id
-          if !available_actions.nil? 
-            resolve_action = available_actions.find {|s| s.name == 'Resolve Issue'}
-          end
-          if !resolve_action.nil?
-            @jira_connection.progressWorkflowAction issue_id, resolve_action.id.to_s, []
-          else
-            Rails.logger.debug "Not allowed to resolve issue #{issue_id}. Allowable actions: "\
-              "#{(available_actions.map {|s| s.name}).to_s}"
-          end
-        rescue StandardError => e
-          Rails.logger.error "Failed to resolve issue #{issue_id} : #{e.to_s}"
-        else
-          Rails.logger.debug "Successfully resolved issue #{issue_id}"
-        end
+      JiraHelper.instance.add_comment issue_id, comment
+      
+      if (regexp_resolve_issue match) && payload["action"] == "closed"
+        JiraHelper.instance.resolve issue_id
       end
     end
     
     render :nothing => true, :status => 200  
   end
+
+  OPENED_COMMENT_TEMPLATE =
+  "(i) [%{user_id}|%{user_url}] referenced this issue in project [%{repo_name}|%{repo_url}]:\n\n"\
+  "*[Pull Request %{pr_number}|%{pr_url}]* _\"%{pr_title}\"_"
   
   def opened_comment_from(json_payload)
     
@@ -94,9 +46,12 @@ class PullRequestController < ApplicationController
         pr_number: json_payload["number"], pr_url: pr["html_url"],
         pr_title: pr["title"] }
   end
-  
-  def closed_comment_from(json_payload)   
 
+  CLOSED_COMMENT_TEMPLATE = 
+  "(/) [%{user_id}|%{user_url}] resolved this issue with *[Pull Request %{pr_number}|%{pr_url}]*"
+      
+  def closed_comment_from(json_payload)   
+  
     pr = json_payload["pull_request"]
     user = pr["user"]
     repo = pr["head"]["repo"]
@@ -106,11 +61,5 @@ class PullRequestController < ApplicationController
         pr_number: json_payload["number"],
         pr_url: pr["html_url"], pr_title: pr["title"] }
   end
-  
-  def find_issue_regexp
-    Regexp.new(FIND_ISSUE_REGEXP_TEMPLATE % {projects: @jira_projects.join('|')},
-      Regexp::IGNORECASE)
-  end
-  
-  
+
 end
---
+++ lib/modules.rb
@@ -0,0 +1,79 @@
+require 'singleton'
+require 'yaml'
+require 'jira4r'
+
+module Modules
+     
+  class JiraHelper
+
+    include Singleton
+    
+    FIND_ISSUE_REGEXP_TEMPLATE = 
+       "(fixe?[s|d] *|resolve[s|d]? *|close[s|d]? *)?(http[^ ]+/)?(%{projects})-([0-9]+)"
+
+    def initialize
+      @jira_config = YAML.load(File.new "config/config.yml", 'r')
+      @jira_projects = @jira_config['projects']
+      @jira_connection = Jira4R::JiraTool.new(2, @jira_config['address'])
+
+      # Optional SSL parameters
+      if @jira_config['ssl_version'] != nil
+        @jira_connection.driver.streamhandler.client.ssl_config.ssl_version = @jira_config['ssl_version']
+      end
+      if @jira_config['verify_certificate'] == false
+        @jira_connection.driver.options['protocol.http.ssl_config.verify_mode'] = OpenSSL::SSL::VERIFY_NONE
+      end
+
+      @jira_connection.login(@jira_config['username'], @jira_config['password'])
+      @regexp = Regexp.new(FIND_ISSUE_REGEXP_TEMPLATE % {projects: @jira_projects.join('|')},
+        Regexp::IGNORECASE)
+    end
+
+    def add_comment(issue_id, body)
+      
+      rc = Jira4R::V2::RemoteComment.new
+      rc.body = body
+      
+      begin
+        @jira_connection.addComment issue_id, rc 
+      rescue
+        Rails.logger.error "Failed to add comment to issue #{issue_id}"
+      else
+        Rails.logger.debug "Successfully added comment to issue #{issue_id}"
+      end
+    end
+    
+    def resolve(issue_id)
+      begin
+        available_actions = @jira_connection.getAvailableActions issue_id
+        if !available_actions.nil? 
+          resolve_action = available_actions.find {|s| s.name == 'Resolve Issue'}
+        end
+        if !resolve_action.nil?
+          @jira_connection.progressWorkflowAction issue_id, resolve_action.id.to_s, []
+        else
+          Rails.logger.debug "Not allowed to resolve issue #{issue_id}. Allowable actions: "\
+            "#{(available_actions.map {|s| s.name}).to_s}"
+        end
+      rescue StandardError => e
+        Rails.logger.error "Failed to resolve issue #{issue_id} : #{e.to_s}"
+      else
+        Rails.logger.debug "Successfully resolved issue #{issue_id}"
+      end
+    end
+
+    def find_issue_regexp
+      @regexp
+    end
+    
+  end
+  
+  def regexp_resolve_issue(match)
+    !match[0].nil?
+  end
+  
+  def regexp_issue_id(match)
+    match[2] + '-' + match[3]
+  end
+
+end
